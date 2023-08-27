9c9c406252563cc5a362f705a6b3c679455a041d
Zhou Binchi <zbinchi@outlook.com>
2023-08-27 12:58:25 +0300

    Finished the 5th tasks.

--- exercise2.py
+++ exercise2.py
@@ -20,11 +20,26 @@ print(rectResult)
 
 # calculate the sum, product, and average of use's inputs
 values = input("Enter three integer values seperated by comma:\n").split(",")
-values = [int(n) for n in values]
+values = [int(value) for value in values]
 valueSum = sum(values)
 valueProd = math.prod(values)
 valueAvg = valueSum / len(values)
 valueResult = "Their sum is {}, their product is {}, and their average is {}.".format(valueSum, valueProd, valueAvg)
 print(valueResult)
 
-#
+# convert medieval units into modern units
+convertRates = [20, 32, 13.3]
+medievalInputs = input("Enter weight in talents, pounds, and lots, seperated by comma:\n").split(",")
+medievalInputs = [float(medievalInput) for medievalInput in medievalInputs]
+sumWeight = 0
+for inputID, medievalInput in enumerate(medievalInputs):
+    for convertRate in convertRates[inputID:]:
+        medievalInput *= convertRate
+    sumWeight += medievalInput
+kilos = sumWeight // 1000
+grams = sumWeight % 1000
+weightResult = "The weight in modern units is:\n{} kilograms and {:.2f} grams".format(kilos, grams)
+print(weightResult)
+
+# draw a random combination of 3 integer between 0 and 9
+
