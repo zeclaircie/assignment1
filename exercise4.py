b1d1ca85ab2f7824b72052e5728e85cd8637a6b8
Zhou Binchi <zbinchi@outlook.com>
2023-09-08 11:11:33 +0300

    Finished task 1-4

---
+++ exercise4.py
@@ -0,0 +1,40 @@
+import random
+
+# print out all numbers divisible by three in the range of 1-1000
+divisible = []
+numerator = 0
+while numerator <= 1000:
+    numerator += 3
+    divisible.append(numerator)
+print(divisible)
+
+# converts inches to centimeters until the user inputs a negative value
+inches = float(input("Enter the inches:\n"))
+while inches >= 0:
+    centimeters = inches * 2.54
+    print(f"{inches} inches is {centimeters} centimeters.\n")
+    inches = float(input("Enter the inches:\n"))
+print("This is a negative value. Program ends.")
+
+# ask user to input numbers till receiving a blank input, and then print max and min
+numList = []
+userInput = input("Enter an number. If you're done, input a blank space to quit.\n")
+while userInput != " ":
+    numList.append(userInput)
+    userInput = input("Enter an number. If you're done, input a blank space to quit.\n")
+numList = [float(num) for num in numList]
+numResult = f"The largest number is {max(numList)} and the smallest number is {min(numList)}."
+print(numResult)
+
+# let the user guess a random number
+answer = random.randrange(1, 11)
+guess = int()
+while guess != answer:
+    guess = int(input("Take a guess of my number. It's between 1 and 10.\n"))
+    if guess < answer:
+        print("Too low!")
+    if guess > answer:
+        print("Too high!")
+print("You are correct!")
+
+# check username and password
