84128fd5c07cb3ac97741393a96b62ee4116655d
Zhou Binchi <zbinchi@outlook.com>
2023-10-24 15:19:30 +0300

    Module 9

--- exercise9/task4.py
+++ exercise9/task4.py
@@ -1,3 +1,6 @@
+import random
+from tabulate import tabulate
+
 class Car:
     def __init__(self, regNum, maxSpeed, crntSpeed=0, distance=0):
         self.regNum = regNum
@@ -18,3 +21,25 @@ class Car:
     def drive(self, hours):
         newDistance = self.crntSpeed * hours
         self.distance += newDistance
+
+
+cars = []
+for i in range(10):
+    regnum = f"ABC-{i+1}"
+    mspeed = random.randrange(100, 200)
+    cars.append(Car(regnum, mspeed))
+
+stop = False
+while not stop:
+    for car in cars:
+        if car.distance >= 10000:
+            stop = True
+        car.accelerate(random.randrange(-10, 15))
+        car.drive(1)
+
+
+tbl = tabulate({"Registration Number": [car.regNum for car in cars],
+                "Maximum Speed": [car.maxSpeed for car in cars],
+                "Current Speed": [car.crntSpeed for car in cars],
+                "Travelled Distance": [car.distance for car in cars]}, headers="keys")
+print(tbl)
